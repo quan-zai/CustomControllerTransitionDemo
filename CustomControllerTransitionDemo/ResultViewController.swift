65b8981d65a4f44ebc5102f5ee0564258bef7420
正权 徐 <xuzhengquan@yundianjia.com>
2016-09-22 18:29:15 +0800

    初始化

---
+++ CustomControllerTransitionDemo/ResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ResultViewController.swift
+//  CustomControllerTransitionDemo
+//
+//  Created by 权仔 on 16/9/22.
+//  Copyright © 2016年 XZQ. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            view.backgroundColor = UIColor.blue
+    }
+}
---
+++ CustomControllerTransitionDemo/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  CustomControllerTransitionDemo
+//
+//  Created by 权仔 on 16/9/22.
+//  Copyright © 2016年 XZQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var tableView : UITableView?
+    
+    var titleArray = ["item 01", "item 02", "item 03", "item 04", "item 05"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView = UITableView(frame: CGRect(x:0, y:0, width:UIScreen.main.bounds.width, height:UIScreen.main.bounds.height), style: .plain)
+        tableView?.delegate = self;
+        tableView?.dataSource = self;
+        view.addSubview(tableView!)
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Action", style: .plain, target: self, action: #selector(rightButtonClick(button:)))
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = titleArray[indexPath.row]
+        return cell
+    }
+    
+    func rightButtonClick(button:UIButton)
+    {
+        present(ResultViewController(), animated: true) { 
+            
+        }
+    }
+    
+}
+
